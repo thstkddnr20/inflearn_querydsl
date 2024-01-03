75c13423e09f0f0f08aba4072b2b0c0a35cd52e5
thstkddnr20 <thstkddnr20@naver.com>
2024-01-03 12:04:17 +0900

    Gradle - Tasks - Other - CompileJava로 Hello의 QHello 생성

---
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,21 @@
+package study.querydsl;
+
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
