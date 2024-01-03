5f50ae5b81f89f87c8c2c56b2cbef01854d08a0f
thstkddnr20 <thstkddnr20@naver.com>
2024-01-03 13:38:53 +0900

    첫 예제로 JPQL문을 querydsl로 바꿔서 생성하는 테스트 작성

---
+++ src/test/java/study/querydsl/entity/QuerydslBasicTest.java
@@ -0,0 +1,67 @@
+package study.querydsl.entity;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+
+    @Autowired
+    EntityManager em;
+
+    JPAQueryFactory queryFactory;
+
+    @BeforeEach
+    public void before(){
+        queryFactory = new JPAQueryFactory(em);//JPAQueryFactory를 만들면서 생성자로 EntityManager를 넘겨준다
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    public void startJPQL(){
+        String qlString = "select m from Member m where m.username =:username";
+
+        Member findMember = em.createQuery(qlString, Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void startQuerydsl(){
+
+        QMember m = new QMember("m"); // 변수명에 별칭 지정
+
+        Member findMember = queryFactory
+                .select(m)
+                .from(m)
+                .where(m.username.eq("member1")) // 파라미터 바인딩을 안해줘도 된다.
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+}
